c688d8bd0345b3fe111214f203786b8435318f26
Quang Nguyễn <mrshelby@Mr-Shelby-2.local>
2020-10-31 21:43:26 +0700

    CameraDemo done

--- CameraDemo/app/src/main/java/com/example/camerademo/MainActivity.java
+++ CameraDemo/app/src/main/java/com/example/camerademo/MainActivity.java
@@ -2,17 +2,39 @@ package com.example.camerademo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.service.media.MediaBrowserService;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
     int REQUEST_CODE_CAMERA = 100;
     Button btnTake;
-    ImageView imgMain;
+    ImageView imgDemo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnTake = (Button) findViewById(R.id.btnTake);
+        imgDemo = (ImageView) findViewById(R.id.imgMain);
+
+        btnTake.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent,REQUEST_CODE_CAMERA);
+            }
+        });
+    }
+    @Override
+    protected void onActivityResult(int requestCode,int resultCode, Intent data){
+        if (requestCode==REQUEST_CODE_CAMERA && resultCode==RESULT_OK && data != null) {
+            Bitmap bitmap = (Bitmap) data.getExtras().get("data");
+        }
+        super.onActivityResult(requestCode,resultCode,data);
     }
 }
